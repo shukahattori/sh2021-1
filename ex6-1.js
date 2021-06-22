1170e794647d3016919db12e097e96440ed0ca3a
shukahattori <zm18067@josai.ac.jp>
2021-06-22 02:08:15 +0000

    0622

---
+++ ex6-1.js
@@ -0,0 +1,7 @@
+function　sayhello61(){
+ alert('Hello,Function!');
+}
+
+function　saygoodbye61(){
+    alert('Goodbye,Function!');
+   }
---
+++ myjs.js
@@ -0,0 +1,3 @@
+// alert('Hello, Shuka!');
+// alert(1+1);
+alert(Math.sqrt(1+1));
